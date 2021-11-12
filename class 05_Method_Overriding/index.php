63d2d734f0bdbd4c87d3886489f58f1d105d5214
devriazul <engr.riazul@gmail.com>
2021-11-13 00:01:23 +0600

    class 5

---
+++ class 05_Method_Overriding/index.php
@@ -0,0 +1,28 @@
+<?php
+
+class teacher{
+        protected $name;
+
+        function __construct($n){
+            $this->name = $n;
+        }
+        function info(){
+            echo "Teacher ";
+            echo "Name: " . $this->name ."<br>";
+        }
+}
+//         class headmaster extends teacher{
+//             public $extra = 10000;
+
+//             function info(){
+//                 $this ->extra += $this->salary;
+//                 echo "Headmaster".'<br>';
+//                 echo $this->name . " = " . $this ->extra ."<br>";
+//             }
+// }
+    $t = new teacher("Riaz",20000);
+    // $t ->name = "Abdullah noman";
+    $t ->info();
+
+
+?>
