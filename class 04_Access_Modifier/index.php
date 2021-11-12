379d8cb6529c4e8100e0408bb2ad9ef345d2ffd5
devriazul <engr.riazul@gmail.com>
2021-11-12 23:53:57 +0600

    class 4

--- class 04_Access_Modifier/index.php
+++ class 04_Access_Modifier/index.php
@@ -2,32 +2,27 @@
 
 class teacher{
         public $name;
-        public $salary;
 
-        function __construct($n,$s){
+        function __construct($n){
             $this->name = $n;
-            $this->salary = $s;
         }
         function info(){
-            echo "Teacher".'<br>';
-            echo $this->name . " = " .$this->salary."<br>";
+            echo "Teacher ";
+            echo "Name: " . $this->name ."<br>";
         }
 }
-        class headmaster extends teacher{
-            public $extra = 10000;
+//         class headmaster extends teacher{
+//             public $extra = 10000;
 
-            function info(){
-                $this ->extra += $this->salary;
-                echo "Headmaster".'<br>';
-                echo $this->name . " = " . $this ->extra ."<br>";
-            }
-}
+//             function info(){
+//                 $this ->extra += $this->salary;
+//                 echo "Headmaster".'<br>';
+//                 echo $this->name . " = " . $this ->extra ."<br>";
+//             }
+// }
     $t = new teacher("Riaz",20000);
-    $h = new headmaster("Nahian",25000);
-
+    $t ->name = "Abdullah noman";
     $t ->info();
-    $h -> info();
 
-    //everything is up to date
 
 ?>
