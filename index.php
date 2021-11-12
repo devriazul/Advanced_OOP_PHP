45e36059956e5ebdb5242cba7148e5bc9387d4c1
devriazul <engr.riazul@gmail.com>
2021-11-12 10:33:59 +0600

    change

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
             $this->c = $this->a+$this->b;
             return $this->c;
         }
+        function sub(){
+            $this->c = $this->a - $this->b;
+            return $this->c;
+        }
     }
 
     $r15 = new calculation();
 
     $r15 -> a = 20;
     $r15 -> b = 10;
+
+    echo "Sum for R15: ". $r15 ->sum();
+    echo "<br>";
+
+    $pulsar = new calculation();
+    $pulsar -> a =50;
+    $pulsar -> b =40;
+
+    echo "Sum for Pulsar: ". $pulsar ->sub();
 
 ?>
