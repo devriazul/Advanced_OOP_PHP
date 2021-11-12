ca47828d26b19eb940ad5b334189652bbdc0da29
devriazul <engr.riazul@gmail.com>
2021-11-12 10:27:48 +0600

    change

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
     class calculation{
         public $a, $b, $c;
+
+        function sum(){
+            $this->c = $this->a+$this->b;
+            return $this->c;
+        }
     }
+
+    $r15 = new calculation();
+
+    $r15 -> a = 20;
+    $r15 -> b = 10;
 
 ?>
