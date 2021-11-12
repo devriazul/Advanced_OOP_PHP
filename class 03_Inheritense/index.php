fedbc530a0e9aaf164ae780bc8171e783549a79f
devriazul <engr.riazul@gmail.com>
2021-11-12 19:25:16 +0600

    problem arise solved

--- class 03_Inheritense/index.php
+++ class 03_Inheritense/index.php
@@ -14,15 +14,16 @@ class teacher{
         }
 }
         class headmaster extends teacher{
-            public $extra = 10000 + $this->salary;
+            public $extra = 10000;
 
             function info(){
+                $this ->extra += $this->salary;
                 echo "Headmaster".'<br>';
-                echo $this->name . " = " . $extra ."<br>";
+                echo $this->name . " = " . $this ->extra ."<br>";
             }
 }
     $t = new teacher("Riaz",20000);
-    $h = new headmaster("Nahian",20000);
+    $h = new headmaster("Nahian",25000);
 
     $t ->info();
     $h -> info();
