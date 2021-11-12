196707250cb449ddab6770a801e339fc6219bbfe
devriazul <engr.riazul@gmail.com>
2021-11-12 10:21:32 +0600

    change

--- whatisoop.php
+++ whatisoop.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "Object oriented programing = OOP";
+echo "Object oriented programing = OOP"; 
+echo "<br>";
+echo "Reason to use oop, 1. Code Re-Usable, 2. Well Organize, 3. Easier to debug";
 
 ?>
