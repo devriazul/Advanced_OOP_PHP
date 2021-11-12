1569a1f5ec442edac8e40eb991da564ed44fd7bd
devriazul <engr.riazul@gmail.com>
2021-11-12 10:44:09 +0600

    change

--- class 02/index.php
+++ class 02/index.php
@@ -1,31 +1,16 @@
 <?php
 
-    class calculation{
-        public $a, $b, $c;
+    class employee{
+        public $name, $salary;
 
-        function sum(){
-            $this->c = $this->a+$this->b;
-            return $this->c;
-        }
-        function sub(){
-            $this->c = $this->a - $this->b;
-            return $this->c;
+        function info(){
+            echo $this ->name . " = " . $this->salary ."<br>";
         }
     }
+   $person = new employee();
+   $person -> name = "Riazul Islam";
+   $person -> salary = "30000";
 
-    $r15 = new calculation();
-
-    $r15 -> a = 20;
-    $r15 -> b = 10;
-
-    echo "Sum for R15: ". $r15 ->sum();
-    echo "<br>";
-
-    $pulsar = new calculation();
-    $pulsar -> a =50;
-    $pulsar -> b =40;
-
-    echo "Sub for Pulsar: ". $pulsar ->sub();
-   
+   $person -> info();
 
 ?>
